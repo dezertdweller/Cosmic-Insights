441c1992cb3c2c4cad357e3fc95ae532b13287fc
Katia Lopes-Gilbert <katialopesgilbert@gmail.com>
2025-08-24 11:41:00 -0700

    Co-authored-by: ashloong <ashloong@users.noreply.github.com> Co-authored-by: EshaSayaji <EshaSays@users.noreply.github.com> Co-authored-by: Kathryn Kashitsyn <KathrynKashitsyn@users.noreply.github.com>

---
+++ query_run.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+import json
+
+
+print("Hello, World!")
+
+print("Hello, World! -Ashley")
+
+print("Hello, World! -Ashley")
+
+print("Hello, World! -Ashley")
+
+
+print("Hello, World - Esha")
+print("Hello, World - Esha")
+
